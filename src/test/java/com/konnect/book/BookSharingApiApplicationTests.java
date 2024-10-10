e19b624b2e503bde3882747cef42316c87ffb979
Akyy03 <akraaam003@gmail.com>
2024-10-10 14:27:53 +0300

    Added some security

---
+++ src/test/java/com/konnect/book/BookSharingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konnect.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSharingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
